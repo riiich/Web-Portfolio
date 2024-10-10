b8212e4c74b3090ce6d9c8e7e685e6a13e1faa18
Rich <horich21@gmail.com>
2024-10-10 00:52:16 -0700

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,3 @@
-import aboutImg from "../assets/images/aboutPlaceholder.png";
 import moontrekImg from "../assets/images/project-images/moontrek2.jpg";
 import mymusicwrapImg from "../assets/images/project-images/mymusicwrap.png";
 import eventTracerImg from "../assets/images/project-images/eventTracer.png";
@@ -42,15 +41,7 @@ export const PROJECTS = [
 			"A platform for people to create and host events. Browse and purchase tickets to " +
 			"publicly hosted events. ",
 		timeFrame: "June 2024 - August 2024",
-		technologies: [
-			"Next.js",
-			"MongoDB",
-			"TailwindCSS",
-			"Clerk",
-			"Stripe",
-			"shadcn",
-			"uploadthing",
-		],
+		technologies: ["Next.js", "MongoDB", "TailwindCSS", "Clerk", "Stripe", "shadcn", "uploadthing"],
 		image: eventTracerImg,
 		link: "https://event-tracer.vercel.app/",
 		githubLink: "https://github.com/riiich/EventTracer",
@@ -108,7 +99,8 @@ export const EXPERIENCES = [
 		dateEmployed: "09/2024 - Present",
 		description: [
 			"Digitized and organized physical copies of historical files, ensuring accurate categorization and integration into the claims database.",
-			"Tracked the progress of scanned files using Excel, maintaining the latest records for efficient claims processing by adjusters."],
+			"Tracked the progress of scanned files using Excel, maintaining the latest records for efficient claims processing by adjusters.",
+		],
 	},
 	{
 		id: 2,
